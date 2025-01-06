882cc6b3bede93daba19ec3d0864db7b8e33068b
codesche <codesche@gmail.com>
2025-01-06 23:18:40 +0900

    [PRO-Lv1] 수박수박수박수박수박수?

---
+++ src/algo250106/Pro12922.java
@@ -0,0 +1,25 @@
+package algo250106;
+
+// 박
+public class Pro12922 {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        String str1 = "수";
+        String str2 = "박";
+
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                sb.append(str1);
+            } else {
+                sb.append(str2);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Pro12922 T = new Pro12922();
+        System.out.println(T.solution(3));
+    }
+}
