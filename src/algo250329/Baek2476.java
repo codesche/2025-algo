d1ff817608543f9b3808b280aed4252be72193e6
codesche <codesche@gmail.com>
2025-03-29 22:36:39 +0900

    [백준-브론즈 3] 주사위게임

---
+++ src/algo250329/Baek2476.java
@@ -0,0 +1,41 @@
+package algo250329;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 - 주사위게임 (브론즈 3)
+public class Baek2476 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int total = 0;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == b && b == c) {
+                total = 10000 + a * 1000;
+            } else if (a == b || a == c) {
+                total = 1000 + a * 100;
+            } else if (b == c) {
+                total = 1000 + b * 100;
+            } else {
+                int temp = Math.max(a, b);
+                int maxValue = Math.max(temp, c);
+                total = maxValue * 100;
+            }
+
+            if (max < total) {
+                max = total;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
