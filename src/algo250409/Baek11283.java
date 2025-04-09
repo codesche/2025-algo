7d5a68bdae728775a61e52a8988773be9be5f642
codesche <codesche@gmail.com>
2025-04-09 21:08:12 +0900

    [백준-브론즈 4] 한글 2

---
+++ src/algo250409/Baek11283.java
@@ -0,0 +1,11 @@
+package algo250409;
+
+import java.io.*;
+
+// 백준 - 한글 2 (브론즈 4)
+public class Baek11283 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine().charAt(0) - 44031);
+    }
+}
