2d31386219fdd06a1f2b61a2b2166f61052239f3
codesche <codesche@gmail.com>
2025-04-22 23:27:55 +0900

    [Softeer-Lv1] 효도의 시작

---
+++ src/algo250422/Softeer7724.java
@@ -0,0 +1,10 @@
+package algo250422;
+
+// 효도의 시작 - Lv1
+public class Softeer7724 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("엄마 아빠 사랑해요!");
+        System.out.print(sb);
+    }
+}
