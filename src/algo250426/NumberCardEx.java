016b1a9a670efbc95f19e5642515789deb7780ac
codesche <codesche@gmail.com>
2025-04-27 00:26:12 +0900

    숫자 카드 찾기

---
+++ src/algo250426/NumberCardEx.java
@@ -0,0 +1,63 @@
+package algo250426;
+
+// 숫자 카드 찾기
+/*
+문제 설명
+N개의 숫자가 적힌 카드 더미가 있습니다.
+M개의 숫자가 주어질 때, 각 숫자가 카드 더미에 존재하는지 O(1) 시간에 확인하세요.
+
+입력 형식
+첫째 줄: 카드 개수 N (1 ≤ N ≤ 500,000)
+둘째 줄: 카드에 적힌 숫자 N개 (-10⁷ ~ 10⁷)
+셋째 줄: 확인할 숫자 개수 M (1 ≤ M ≤ 500,000)
+넷째 줄: 확인할 숫자 M개
+
+출력 형식
+확인할 숫자에 대해, 카드 더미에 존재하면 1, 존재하지 않으면 0을 출력합니다.
+
+입력:
+5
+6 3 2 10 -10
+8
+10 9 -5 2 3 4 5 -10
+
+출력:
+1 0 0 1 1 0 0 1
+
+ */
+
+import java.io.*;
+import java.util.*;
+public class NumberCardEx {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        int[] numbers = new int[M];
+        for (int k = 0; k < M; k++) {
+            numbers[k] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] answer = new int[M];
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int arrValue : arr) {
+                if (numbers[i] == arrValue) {
+                    answer[i]++;
+                }
+            }
+        }
+
+        for (int j : answer) {
+            System.out.print(j + " ");
+        }
+    }
+}
