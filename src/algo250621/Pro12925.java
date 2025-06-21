540fb76cecf19fd368a08cc01bb34de4b7a29b39
codesche <codesche@gmail.com>
2025-06-21 16:37:54 +0900

    [Pro-Lv1] 문자열을 정수로 바꾸기

---
+++ src/algo250621/Pro12925.java
@@ -0,0 +1,12 @@
+package algo250621;
+
+// 프로그래머스 - 문자열을 정수로 바꾸기 (Lv1)
+public class Pro12925 {
+    public int solution(String s) {
+        int answer = 0;
+
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
