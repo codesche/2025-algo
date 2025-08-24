edd4ca102f087f3697ef9456de207bcff9e2d29f
codesche <codesche@gmail.com>
2025-08-25 08:33:15 +0900

    [Pro-Lv2] 가장 큰 수

---
+++ src/algo250825/Pro42746.java
@@ -0,0 +1,29 @@
+package algo250825;
+
+import java.util.*;
+
+// 프로그래머스 - 가장 큰 수 (Lv2)
+public class Pro42746 {
+    public String solution(int[] numbers) {
+        StringBuilder sb = new StringBuilder();
+        String[] arr = new String[numbers.length];
+        int length = arr.length;
+
+        // 문자열 배열 요소 삽입
+        for (int i = 0; i < length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        // 비교하는 두 숫자를 앞, 뒤로 번갈아가며 이어붙인 것을 기준으로 비교
+        Arrays.sort(arr, (o1, o2) -> (o1 + o2).compareTo(o2 + o1));
+
+        // 0이 제일 앞일 경우 "000...." -> "0" 리턴
+        if (arr[0].equals("0")) return "0";
+
+        for (String num : arr) {
+            sb.append(num);
+        }
+
+        return sb.toString();
+    }
+}
