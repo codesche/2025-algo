518da0d2732fdb2778da5d94444cd0163232c9bb
codesche <codesche@gmail.com>
2025-10-21 16:48:47 +0900

    [백준-브론즈 5] Hello World

---
+++ src/algo251021/Baek2557.java
@@ -0,0 +1,12 @@
+package algo251021;
+
+// 백준 - Hello World (브론즈 5)
+// Back to the Basic
+// StringBuilder 를 통해 출력하는 게 성능 최적화 면에서 유리
+public class Baek2557 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World!");
+        System.out.println(sb);
+    }
+}
